251bdaa6826e9d2a4d5f981a8587527e64fc4587
Baptiste Morvan <baptiste@MacBook-Air-de-Baptiste.local>
2022-10-14 16:59:56 +0200

    ok

---
+++ src/helpers/ProjectList.js
@@ -0,0 +1,13 @@
+import Proj1 from "../assets/proj1.jpg";
+import Proj2 from "../assets/proj2.webp";
+
+export const ProjectList = [
+    {
+        name: "Cool projet",
+        image: Proj1,
+    },
+    {
+        name: "Le bon pain",
+        image: Proj2,
+    }
+]
