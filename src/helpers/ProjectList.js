01dc9532857790fadd04369dcc0cdb939ade72ed
Baptiste Morvan <baptiste@MacBook-Air-de-Baptiste.local>
2022-11-14 17:08:25 +0100

    displayItem sticky ok

--- src/helpers/ProjectList.js
+++ src/helpers/ProjectList.js
@@ -8,7 +8,8 @@ export const ProjectList = [
         date: "2022",
         category: "Vitrine",
         context: "Freelance",
-        link: "https://leblaireau-barbier.fr/"
+        link: "https://leblaireau-barbier.fr/",
+        info: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor consequuntur pariatur ipsum explicabo quia, vitae corporis ipsa voluptatibus perferendis facere mollitia, at provident dolore nisi tempora dolorum dignissimos! Veniam, voluptate"
     },
     {
         name: "Le bon pain",
@@ -16,20 +17,23 @@ export const ProjectList = [
         date: "2022",
         category: "Vitrine",
         context: "Freelance",
-        link: "https://baptistem44.github.io/l-autre-pain/"
+        link: "https://baptistem44.github.io/l-autre-pain/",
+        info: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor consequuntur pariatur ipsum explicabo quia, vitae corporis ipsa voluptatibus perferendis facere mollitia, at provident dolore nisi tempora dolorum dignissimos! Veniam, voluptate"
     },
     {
         name: "Portfolio V1",
-        image: Proj2,
+        image: "https://picsum.photos/1000/1200",
         date: "2022",
         context: "Freelance",
-        category: "Portfolio"
+        category: "Portfolio",
+        info: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor consequuntur pariatur ipsum explicabo quia, vitae corporis ipsa voluptatibus perferendis facere mollitia, at provident dolore nisi tempora dolorum dignissimos! Veniam, voluptate"
     },
     {
         name: "Social network",
-        image: Proj2,
+        image: "https://picsum.photos/1000/1200",
         date: "2021",
         context: "Freelance",
-        category: "Learn Project"
+        category: "Learn Project",
+        info: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor consequuntur pariatur ipsum explicabo quia, vitae corporis ipsa voluptatibus perferendis facere mollitia, at provident dolore nisi tempora dolorum dignissimos! Veniam, voluptate"
     },
 ]
--- src/pages/ProjectDisplay.js
+++ src/pages/ProjectDisplay.js
@@ -9,31 +9,48 @@ function ProjectDisplay() {
   const project = ProjectList[id];
   const navigate = useNavigate();
   return (
-    <div >
-      <div className="project_display">
+    <div>
+      <div className="project_display_sticky">
+        <div className="project_display">
         <button className="project_linkback" onClick={() => navigate(-1)}>Go back</button>
-        <div className="project_description">
-          <h1> {project.name}</h1>
-          <article className="project_description_article">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor consequuntur pariatur ipsum explicabo quia, vitae corporis ipsa voluptatibus perferendis facere mollitia, at provident dolore nisi tempora dolorum dignissimos! Veniam, voluptate!
-          </article>
-        </div>
-        <div className="project_content">
-          <img alt="project_content" className="project_content_img" src={project.image} width="100%" height="100%" />
+          <div className="project_all">
+            <div className="project_content_part">
+              <article className="project_description">
+                <h1 className="project_description_h1">{project.name}</h1>
+                <p className="project_description_article">{project.info}</p>
+              </article>
+              <div className="project_content">
+                <img alt="project_content" className="project_content_img" src={project.image} width="100%" height="100%" />
+                <img alt="project_content" className="project_content_img" src={project.image} width="100%" height="100%" />
+              </div>
+            </div>
+            <div className="project_content_part">
+              <article className="project_description">
+                <p className="project_description_article">{project.info}</p>
+              </article>
+              <div className="project_content">
+                <img alt="project_content" className="project_content_img" src={project.image} width="100%" height="100%" />
+              </div>
+            </div>
+          </div>
         </div>
       </div>
-      <footer className="footer">
-        <div className="footer_name">
-          <p> {project.category}</p>
-          <p>/{project.date}</p>
+
+
+
+      <footer className="project_footer">
+          <div className="project_footer_name">
+            <p> {project.category}</p>
+            <p>/{project.date}</p>
+          </div>
+          <div className="project_footer_law">
+            <p> {project.category}</p>
+            <p>/{project.date}</p>
+          </div>
+          <div className="project_footer_link">
+            <a className="project_footer_link_item" target="_blank" rel="noopener noreferrer" href={project.link}> Site in Live</a>  
         </div>
-        <div className="footer_law">
-          <p> {project.category}</p>
-          <p>/{project.date}</p>
-        </div>
-        <div className="footer_link">
-          <a className="footer_link_item" target="_blank" rel="noopener noreferrer" href={project.link}> Site in Live</a>  
-        </div>
+        
       </footer> 
       
     </div>
